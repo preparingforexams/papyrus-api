c8115beb92e22f933da6265d02377fe44b39a4a4
Torben Carstens <git@carstens.tech>
2023-01-09 22:34:29 +0100

    Upgrade phoenix_live_view, phoenix_live_dashboard, gettext, esbuild
    See #5
    See #6
    See #7
    See #8

--- mix.exs
+++ mix.exs
@@ -39,14 +39,14 @@ defmodule PapyrusApi.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix_live_view, "~> 0.18.0"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
+      {:phoenix_live_dashboard, "~> 0.7"},
+      {:esbuild, "~> 0.6", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.18"},
+      {:gettext, "~> 0.21"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
     ]
